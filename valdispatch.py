70b63e2ccc2c98416c123561319d78d046f9c36d
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-27 09:31:12 +0300

    Initial import
    Add valdispatch (class decorator), __main__ guard

---
+++ valdispatch.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from functools import update_wrapper
+
+
+class valdispatch:
+    def __init__(self, func):
+        self.default = func
+        self.dispatch = {}
+        update_wrapper(self, func)
+
+    def register(self, *values):
+        def decorate(func):
+            self.dispatch.update(dict.fromkeys(values, func))
+            return func
+
+        return decorate
+
+    def __call__(self, num, *args):
+        try:
+            return self.dispatch[num](num, *args)
+        except KeyError:
+            return self.default(num, *args)
+
+
+@valdispatch
+def classify(num, msg="something else"):
+    return f"{num} is {msg}"
+
+
+@classify.register(1, 3, 5)
+def _(num, msg="odd"):
+    return f"{num} is {msg}"
+
+
+@classify.register(2, 4, 6)
+def _(num, msg="even"):
+    return f"{num} is {msg}"
+
+
+if __name__ == "__main__":
+    print(classify(3))
+    print(classify(4))
+    print(classify(100))
