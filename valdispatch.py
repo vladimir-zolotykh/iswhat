38301bb73132eb1918268ac0012e5592e13d82be
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-27 10:13:51 +0300

    Refactor with types
    +DispatchType = Callable[[int, str], str]
    +RegisterResultType = Callable[[DispatchType], DispatchType]

--- valdispatch.py
+++ valdispatch.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Callable
 from functools import update_wrapper
+
+DispatchType = Callable[[int, str], str]
+RegisterResultType = Callable[[DispatchType], DispatchType]
 
 
 class valdispatch:
-    def __init__(self, func):
+    def __init__(self, func: DispatchType):
         self.default = func
-        self.dispatch = {}
+        self.dispatch: dict[int, DispatchType] = {}
         update_wrapper(self, func)
 
-    def register(self, *values):
-        def decorate(func):
+    def register(self, *values: int) -> RegisterResultType:
+        def decorate(func: DispatchType) -> DispatchType:
             self.dispatch.update(dict.fromkeys(values, func))
             return func
 
         return decorate
 
-    def __call__(self, num, *args):
+    def __call__(self, num: int, *args: str) -> str:
         try:
             return self.dispatch[num](num, *args)
         except KeyError:
@@ -25,17 +29,17 @@ class valdispatch:
 
 
 @valdispatch
-def classify(num, msg="something else"):
+def classify(num: int, msg: str = "something else") -> str:
     return f"{num} is {msg}"
 
 
 @classify.register(1, 3, 5)
-def _(num, msg="odd"):
+def _(num: int, msg: str = "odd") -> str:
     return f"{num} is {msg}"
 
 
 @classify.register(2, 4, 6)
-def _(num, msg="even"):
+def _(num: int, msg: str = "even") -> str:
     return f"{num} is {msg}"
 
 
