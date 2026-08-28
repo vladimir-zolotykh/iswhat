e1ea2a05fbe496a861310b80a524e18641814a80
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-28 09:49:46 +0300

    Initial import
    Add Singleton, Module, main guard

---
+++ singleton.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        name = args[0]
+        if cls in Singleton._instances:
+            if name in Singleton._instances[cls]:
+                return Singleton._instances[cls][name]
+            else:
+                obj = super().__call__(*args, **kwargs)
+                Singleton._instances[cls][name] = obj
+                return obj
+        else:
+            obj = super().__call__(*args, **kwargs)
+            name = args[0]
+            Singleton._instances[cls] = {name: obj}
+            return obj
+
+
+class Module(metaclass=Singleton):
+    def __init__(self, name: str = "functools"):
+        print(f"Initializing module {name}")
+        self.name = name
+
+
+if __name__ == "__main__":
+    m1 = Module("struct")
+    m2 = Module("struct")
+    assert m1 is m2
